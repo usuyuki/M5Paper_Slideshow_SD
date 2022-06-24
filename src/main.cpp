83f43a0bbc96af2282c1b64c77f5f455af44650f
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-06-24 16:41:44 +0900

    wip フォーマッタと微調整(若干フォーマットがきになるけど(elseとか……)諦め

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,103 @@
 #include <M5EPD.h>
 File root;
 M5EPD_Canvas canvas(&M5.EPD);
-static char* file_name_pointers[100];//文字列へのポインタの配列
-static int n_file = 0;//ファイル数
-static int selected_file_number=0;//ファイル位置を保持
+static char *file_name_pointers[100]; //文字列へのポインタの配列
+static int n_file = 0;                //ファイル数
+static int selected_file_number = 0;  //ファイル位置を保持
 
 /**
-* 文字列が特定の文字列で終わるか？ 
-*/
-bool ends_with(const std::string& str, const std::string& suffix) {
+ * 文字列が特定の文字列で終わるか？
+ * 参考記事:https://maku77.github.io/cpp/string/ends_with.html
+ */
+bool ends_with(const std::string &str, const std::string &suffix)
+{
   size_t len1 = str.size();
   size_t len2 = suffix.size();
   return len1 >= len2 && str.compare(len1 - len2, len2, suffix) == 0;
 }
 
 /**
-* 文字列を辞書順にソート
-*/
+ * 文字列を辞書順にソート
+ * 天下のバブルソートです。
+ */
 void SortByName()
 {
-	int i, j;
-	char* buf;//比較時逃がす
+  int i, j;
+  char *buf; //比較時逃がす
 
-	for (i = 0; i < n_file - 1; i++) {
-		//整列済みの箇所は探索不要なので(j>i)
-		for (j = n_file - 1; j > i; j--) {
-			if (strcmp(file_name_pointers[j-1], file_name_pointers[j]) > 0) {
-				buf = file_name_pointers[j];
-				file_name_pointers[j] = file_name_pointers[j - 1];
-				file_name_pointers[j - 1] = buf;
-			}
-		}
-	}
+  for (i = 0; i < n_file - 1; i++)
+  {
+    //整列済みの箇所は探索不要なので(j>i)
+    for (j = n_file - 1; j > i; j--)
+    {
+      if (strcmp(file_name_pointers[j - 1], file_name_pointers[j]) > 0)
+      {
+        buf = file_name_pointers[j];
+        file_name_pointers[j] = file_name_pointers[j - 1];
+        file_name_pointers[j - 1] = buf;
+      }
+    }
+  }
 }
 
 /**
-* .jpgのファイル名のポインタをfile_name_pointers配列に格納
-*/
-void roadDirectory(File dir) {
-  int i=0;
+ * .jpgのファイル名のポインタをfile_name_pointers配列に格納
+ * mallocで可変長文字列のポインタ配列を生成
+ * 終了までずっと使う文字列なのでfreeはしていない←これで良いの……？
+ */
+void roadDirectory(File dir)
+{
+  int i = 0;
   const char *name;
-  while(true) {
-    File entry =  dir.openNextFile();
-    if (! entry) {
+  while (true)
+  {
+    File entry = dir.openNextFile();
+    if (!entry)
+    {
       dir.rewindDirectory();
       break;
     }
     name = entry.name();
-    Serial.printf("file_name:%s\n",name);
-    if(ends_with(name,".jpg") || ends_with(name,".JPG")){
-      file_name_pointers[i] = (char*)malloc(strlen(name) + 1);//文字列へのポインタを確保
-      strcpy(file_name_pointers[i], name);//文字列へのポインタをコピー
+    Serial.printf("file_name:%s\n", name);
+    if (ends_with(name, ".jpg") || ends_with(name, ".JPG"))
+    {
+      file_name_pointers[i] = (char *)malloc(strlen(name) + 1); //文字列へのポインタを確保
+      strcpy(file_name_pointers[i], name);                      //文字列へのポインタをコピー
       i++;
     }
   }
   n_file = i;
   // 配列をソート(デフォルトでは不思議な順番になるので名前順に変更)
   SortByName();
-  
 }
 
-/** 
-* ファイル読み込み確認用
-*/
+/**
+ * ファイル読み込み確認用
+ */
 void PrintLines(int num)
 {
-	int i;
-	for (i = 0; i < num; i++){
-		Serial.printf("%s\n", file_name_pointers[i]);
+  int i;
+  for (i = 0; i < num; i++)
+  {
+    Serial.printf("%s\n", file_name_pointers[i]);
   }
 }
 
-/** 
-* 指定された位置のファイルを描画
-*/
-void drawImg(int number){
-  Serial.printf("drawing%d:%s\n", number,file_name_pointers[number]);
-  canvas.drawJpgFile(SD,file_name_pointers[number]); 
-  canvas.pushCanvas(0,0,UPDATE_MODE_GC16);
+/**
+ * 指定された位置のファイルを描画
+ */
+void drawImg(int number)
+{
+  Serial.printf("drawing%d:%s\n", number, file_name_pointers[number]);
+  canvas.drawJpgFile(SD, file_name_pointers[number]);
+  canvas.pushCanvas(0, 0, UPDATE_MODE_GC16);
 }
 
-/** 
-* バッテリー確認処理
-*/
+/**
+ * バッテリー確認処理
+ * 参考記事：https://qiita.com/RAWSEQ/items/21953059ca283486e55b
+ * 参考記事：https://qiita.com/Fitso/items/5f0a94ba1942c74a3d08
+ */
 int Chk_battery()
 {
   char buf[20];
@@ -104,27 +119,27 @@ int Chk_battery()
   {
     battery = 1;
   }
-  //uint8_t px = battery * 25;
-  // sprintf(buf, "BATT%d%% ", (int)(battery * 100));
-  // canvas.drawString(buf, 450, 0);
-
   return (int)(battery * 100);
 }
 
-/** 
-* スライドショーにして良いファイルかを判定する
-*/
-bool is_slide(int slideshow_number){
-  if(ends_with(file_name_pointers[slideshow_number],"_noslide.jpg") || ends_with(file_name_pointers[slideshow_number],"_noslide.JPG")){
+/**
+ * スライドショーにして良いファイルかを判定する
+ */
+bool is_slide(int slideshow_number)
+{
+  if (ends_with(file_name_pointers[slideshow_number], "_noslide.jpg") || ends_with(file_name_pointers[slideshow_number], "_noslide.JPG"))
+  {
     return false;
-  }else{
+  }
+  else
+  {
     return true;
   }
 }
 
-/** 
-* Arduino共通のセットアップ処理
-*/
+/**
+ * Arduino共通のセットアップ処理
+ */
 void setup()
 {
   M5.begin();
@@ -139,14 +154,15 @@ void setup()
   root = SD.open("/");
   roadDirectory(root);
 
-  if(n_file ==0){
-    // ファイル無いときのエラーハンドリング大変なので停止せる
+  if (n_file == 0)
+  {
+    // jpg画像が無いときのエラーハンドリング大変なので停止せる
     Serial.println("no file");
     canvas.setTextSize(2);
     canvas.drawString("File Naiyo Exception", 45, 350);
 
     // 気温取得
-    M5.SHT30.Begin();//温度計周り
+    M5.SHT30.Begin(); //温度計周り
     M5.SHT30.UpdateData();
     float tem = M5.SHT30.GetTemperature();
     float hum = M5.SHT30.GetRelHumidity();
@@ -161,60 +177,75 @@ void setup()
     canvas.pushCanvas(0, 0, UPDATE_MODE_GC16);
     delay(2000);
     //終了
-    M5.shutdown(86400);//1日停止
-
-  }else{
+    M5.shutdown(86400); // 1日停止
+  }
+  else
+  {
+    // jpg画像が存在するとき
     PrintLines(n_file);
     drawImg(0);
   }
 }
 
-void loop() {
+void loop()
+{
   int slideshow_number;
 
   M5.update();
 
-    // バッテリー残量取得
+  // バッテリー残量取得
   int btLevel = Chk_battery();
 
   //充電中は画面更新しないようにする
-  if(btLevel==1){
+  if (btLevel == 1)
+  {
     //充電時の処理
     canvas.createCanvas(540, 960);
     canvas.setTextSize(6);
     canvas.drawString("Charging....", 45, 350);
     canvas.pushCanvas(0, 0, UPDATE_MODE_GC16);
     delay(60000);
-  }else{
+  }
+  else
+  {
     //ファイル位置設定
-    if(M5.BtnL.isPressed()){
-      //37番ピンに対応(上ボタン)
+    if (M5.BtnL.isPressed())
+    {
+      // 37番ピンに対応(上ボタン)
 
       selected_file_number++;
-      if(selected_file_number>(n_file-1)){
+      if (selected_file_number > (n_file - 1))
+      {
         //末端まで行ったら最初に戻る
-        selected_file_number=0;
+        selected_file_number = 0;
       }
       drawImg(selected_file_number);
       delay(1000);
-    }else if(M5.BtnR.isPressed()){
-      //39番ピンに対応(下ボタン)
+    }
+    else if (M5.BtnR.isPressed())
+    {
+      // 39番ピンに対応(下ボタン)
 
       selected_file_number--;
-      if(selected_file_number<0){
+      if (selected_file_number < 0)
+      {
         //先端まで行ったら最後に戻る
-        selected_file_number=(n_file-1);
+        selected_file_number = (n_file - 1);
       }
       drawImg(selected_file_number);
       delay(500);
-    }else if(M5.BtnP.isPressed()){
-      slideshow_number= selected_file_number+1;
+    }
+    else if (M5.BtnP.isPressed())
+    {
+      slideshow_number = selected_file_number + 1;
       Serial.printf("Enable:slideshowMode\n");
       //スライドショーモード
-      while(true){
+      while (true)
+      {
         //上でインクリメントしてるので==n_file-1でなく>=にしてる
-        slideshow_number = (slideshow_number>=(n_file-1)) ? 0 : slideshow_number+1;
-        if(is_slide(slideshow_number)){
+        slideshow_number = (slideshow_number >= (n_file - 1)) ? 0 : slideshow_number + 1;
+        if (is_slide(slideshow_number))
+        {
           drawImg(slideshow_number);
           delay(10000);
         }
